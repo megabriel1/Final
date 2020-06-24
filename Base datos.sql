362608cd50e13bd3b1a0b0d9de6d21615aafdeb5
Edson <edson_gabriel007@hotmail.com>
2020-06-23 20:04:08 -0600

    Base de datos limpia

--- Base datos.sql
+++ Base datos.sql
@@ -8,15 +8,6 @@ ID_categoria int not null,
 foreign key (ID_categoria) references categoria (ID_categoria)
 );
 
-drop table nombre_producto;
-drop table productos;
-drop table categoria;
-
-select a.nombre_producto, b.nombre_categoria from nombre_producto a, categoria b where b.ID_categoria like 1;
-
-select * from nombre_producto;
-select * from categoria;
-
 insert into nombre_producto (nombre_producto, ID_categoria) values 
 ("zucaritas", 1),
 ("corn flakes", 1),
@@ -34,16 +25,6 @@ ID_categoria int primary key not null,
 nombre_categoria varchar(40) not null
 );
 
-drop table categoria;
-
-create table productos(
-ID_productos int,
-ID_nombre int, 
-ID_categoria int,
-foreign key (ID_nombre) references nombre_producto (ID_nombre),
-foreign key (ID_categoria) references categoria (ID_categoria)
-);
-
 insert into categoria (ID_categoria, nombre_categoria) values 
 (1, "cereales"),
 (2, "lacteos"),
@@ -54,7 +35,3 @@ insert into categoria (ID_categoria, nombre_categoria) values
 (7, "carnes"),
 (8, "jugos"),
 (9, "especias");
-
-select * from categoria;
-drop table nombre_producto;
-
